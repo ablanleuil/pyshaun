a8d4305bef59583cae1a8b71032c1e4649a5ae7a
ablanleuil <arthur.blanleuil@gmail.com>
2017-09-20 11:55:55 +0200

    Version fix

--- pyshaun/__init__.py
+++ pyshaun/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __all__ = [ 'load', 'load_file', 'dump', 'Numeric' ]
 
 from .decoder import SHAUNDecoder
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
         name = 'pyshaun',
         packages = ['pyshaun'],
-        version = '0.1.0',
+        version = '0.1.1',
         description = 'SHAUN implementation',
         author = 'Arthur Blanleuil',
         author_email = 'arthur.blanleuil@gmail.com',
